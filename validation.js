5708657ffa62ceb09053ebf54e0d5f4394e5bbcf
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-06-15 20:45:48 +0530

    add validation for phone number continue

--- validation.js
+++ validation.js
@@ -1,4 +1,23 @@
+const inputs = document.querySelectorAll('input');
+
+
 const patterns = {
 
     telephone: /^\d{11}$/
 };
+
+//validation function
+function validate(field, regex){
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    }
+    else
+        field.className = 'invalid';
+}
+
+inputs.forEach((item) => {
+    item.addEventListener('keyup', (e) => {
+        // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]); 
+    });
+});
